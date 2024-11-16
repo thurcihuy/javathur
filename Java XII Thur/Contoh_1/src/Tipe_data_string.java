4e7b67d50b204bedc7473c0833440bdee66033bc
thurcihuy <m.fathurohman88@smk.belajar.id>
2024-11-16 12:47:43 +0700

    Add files via upload

---
+++ Java XII Thur/Contoh_1/src/Tipe_data_string.java
@@ -0,0 +1,10 @@
+class Tipe_data_string {
+  public static void main(String[] args) {
+    String firtName = "Thur";
+    String lastName = "Liebert";
+    
+    System.out.println(firtName);
+    System.out.println(lastName);
+  }
+
+}
---
+++ Java XII Thur/Contoh_2/src/java_length.java
@@ -0,0 +1,9 @@
+class java_length {
+  public static void main(String[] args){
+    String Nama1 = "M Fathurohman";
+    String Nama2 = "Thur Liebert";
+    
+    System.out.println("Panjang String pada Nama 1 adalah : " + Nama1.length());
+    System.out.println("Panjang String pada Nama 1 adalah : " + Nama2.length());
+  }
+}
